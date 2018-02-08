655301b0937209716723f680e1bc5f1a8596f216
henkeldi <henkeldi@gmx.de>
2018-02-08 17:18:12 +0100

    Added documentation

--- media.py
+++ media.py
@@ -1,10 +1,16 @@
 """
-Movies
+Represents a Movie
 """
 
 
 class Movie(object):
-    """Represents a Movie"""
+    """Represents a Movie
+    
+    Args:
+        title (str): The title of the movie
+        poster_image_url (str): An URL which points to a poster of the movie
+        trailer_youtube_url (str): A Youtube URL which points to the movies trailer
+    """
 
     def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title
